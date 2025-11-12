f1b3da250d3de9deb64f529f4dd8c9b22987bf1a
zainab <hlalzynbhlal@gmail.com>
2025-11-12 19:51:19 +0200

    update

--- app/artilcles/page.tsx
+++ app/artilcles/page.tsx
@@ -1,3 +1,14 @@
 export default function page() {
-  return <div>article page</div>;
+  return (
+    <main className="flex flex-col items-center justify-center  p-6 pt-[150px]">
+      <h1 className="text-4xl font-bold text-blue-50 mb-4">
+        Welcome to artilcles page
+      </h1>
+      <p className="text-gray-100 text-center max-w-xl">
+        This is the artilcles page of your project. You can navigate to other
+        pages to view posts, details, and more content. Enjoy exploring the
+        site!
+      </p>
+    </main>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <main className="flex flex-col items-center justify-center  p-6 pt-[150px]">
+      <h1 className="text-4xl font-bold text-blue-50 mb-4">
+        Welcome to My Website
+      </h1>
+      <p className="text-gray-100 text-center max-w-xl">
+        This is the home page of your project. You can navigate to other pages
+        to view posts, details, and more content. Enjoy exploring the site!
+      </p>
+    </main>
+  );
 }
